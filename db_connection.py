804560749e6faac85d19162d7388be3f58b3ec49
emis_project <anushareddy947>
2022-02-28 02:06:01 +0000

    path to directory updated

--- db_connection.py
+++ db_connection.py
@@ -3,6 +3,7 @@ import mysql.connector
 # Connecting from the server
 conn = mysql.connector.connect(user='root',
                                password='abc',
-                               host='127.0.0.1',
+                               host='localhost',
                                database='json_data',
                                auth_plugin='mysql_native_password')
+
